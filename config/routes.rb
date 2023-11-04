ce4441e065ba72d742cfb3bb0f7a31b19a9cc34e
Nazar Volynets <93925872+setwith@users.noreply.github.com>
2023-11-04 20:54:11 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :users, except: %i[update destroy] do
+        put :update, on: :collection
+        delete :destroy, on: :collection
+      end
       post '/login', to: 'authentication#login'
       get '/my_profile', to: 'users#show_current_user'
     end
