e14518fb76449f4340e5a660a1b1e4b758255df7
Nazar Volynets <93925872+setwith@users.noreply.github.com>
2023-11-05 00:28:10 +0200

    add description to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_04_205228) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_04_222105) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_04_205228) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   add_foreign_key "hobbies", "users"
