4cb3833137a73611237c8bf25ffbd3cc76e5aab6
Nazar Volynets <93925872+setwith@users.noreply.github.com>
2023-11-05 00:31:48 +0200

    update user controller

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class UserSerializer < ActiveModel::Serializer
-      attributes :id, :first_name, :last_name
+      attributes :id, :first_name, :last_name, :description
     end
   end
 end
