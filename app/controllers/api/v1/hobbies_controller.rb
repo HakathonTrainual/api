02d16e47de06a2533793b4dde8b392632ed1dc0a
Nazar Volynets <93925872+setwith@users.noreply.github.com>
2023-11-05 03:46:10 +0200

    update user controller

--- app/controllers/api/v1/hobbies_controller.rb
+++ app/controllers/api/v1/hobbies_controller.rb
@@ -1,48 +1,32 @@
 class Api::V1::HobbiesController < ApplicationController
-  before_action :find_user, only: :users_hobbies
-  before_action :find_hobby, only: %i[show edit update destroy]
+  before_action :find_hobby, only: :destroy
 
   def index
     @hobbies = Hobby.all
     render json: @hobbies
   end
 
-  def users_hobbies
-    @hobbies = @user.hobbies
-    render json: @hobbies
-  end
-
   def create
-    @hobby = create_hobby(hobby_params[:name])
-    assign_hobby_to_user(@user, @hobby)
+    @hobby = Hobby.new(hobby_params)
+    if @hobby.save
+      render json: @hobby, status: :created
+    else
+      render json: { errors: @hobby.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    @user.hobbies.delete(@hobby)
+    @hobby.destroy
     head :no_content
   end
 
   private
 
-  def find_user
-    @user = User.find(params[:user_id])
-  end
-
-  def find_hobby
-    @hobby = Hobby.find(params[:id])
-  end
-
   def hobby_params
     params.require(:hobby).permit(:name)
   end
 
-  def create_hobby(name)
-    Hobby.find_or_create_by(name: name)
-  end
-
-  def assign_hobby_to_user(user, hobby)
-    user.hobbies << hobby
-    user.save
-    render json: hobby, status: :created
+  def find_hobby
+    @hobby = Hobby.find(params[:id])
   end
 end
