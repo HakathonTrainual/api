150c8b4e45da111881c1c461b6ff5725aa972de7
VladFomenko <vlad.fomenko.ua@gmail.com>
2023-11-05 10:17:41 +0200

    upd logic

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -10,14 +10,15 @@ class Api::V1::GamesController < ApplicationController
     all_users.each do |user|
       next if user == current_user
       progress = current_user.progresses.find_by(target_user_id: user.id)
-      debugger
       random_users << user if progress.nil? || progress.percentage < 100
       break if random_users.length >= 4
     end
 
     random_users[0].status = true
 
-    result_string = "#{random_users[0].hobbies.join(', ')}: #{random_users[0].description}"
+    result_string = "#{random_users[0].user_hobbies.join(', ')}: #{random_users[0].description}"
+
+    puts(result_string)
 
     res = {
       question: generate_question(result_string),
