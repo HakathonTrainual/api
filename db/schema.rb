8193ebdc74b69e00b58b96407960bda853cc94f7
VladFomenko <vlad.fomenko.ua@gmail.com>
2023-11-05 03:35:21 +0200

    add status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_04_224238) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_05_004101) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,6 +78,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_04_224238) do
     t.integer "department", default: 0, null: false
     t.string "instagram"
     t.string "facebook"
+    t.boolean "status", default: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
